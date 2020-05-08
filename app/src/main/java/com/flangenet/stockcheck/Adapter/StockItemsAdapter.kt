302435a82e4d0911b69dcae0e5dc2188cb5dd789
Wak <LeeTenerife@GMail.Com>
2020-05-08 13:11:26 +0100

    Number 1

---
+++ app/src/main/java/com/flangenet/stockcheck/Adapter/StockItemsAdapter.kt
@@ -0,0 +1,61 @@
+package com.flangenet.stockcheck.Adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.flangenet.stockcheck.Model.StockCheck
+import com.flangenet.stockcheck.Model.StockItem
+import com.flangenet.stockcheck.R
+import kotlinx.android.synthetic.main.stock_line.view.*
+
+class StockItemsAdapter (val context: Context, val items:ArrayList<StockCheck>, val itemClick:(StockCheck) -> Unit):RecyclerView.Adapter<StockItemsAdapter.ViewHolder>(){
+
+    inner class ViewHolder (itemView: View, val itemClick: (StockCheck) -> Unit) : RecyclerView.ViewHolder(itemView){
+        private val description: TextView = itemView.findViewById<TextView>(R.id.Description)
+        private val iD: TextView = itemView.findViewById<TextView>(R.id.txtId)
+        private val stock: TextView = itemView.findViewById<TextView>(R.id.txtStock)
+
+        fun bindItems(item:StockCheck){
+            description.text = item.description
+            iD.text = item.displayOrder.toString()
+
+            stock.text = String.format("%.2f", item.stock)
+
+            itemView.setOnClickListener{
+                //it.setBackgroundColor(context.getColor(android.R.color.holo_green_light))
+                itemClick(item)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StockItemsAdapter.ViewHolder {
+       // println("*****$viewType")
+        val view = LayoutInflater.from(context).inflate(
+            R.layout.stock_line,parent, false)
+        return ViewHolder(view,itemClick)
+    }
+
+    override fun getItemCount(): Int {
+        return items.count()
+    }
+
+    override fun onBindViewHolder(holder: StockItemsAdapter.ViewHolder, position: Int) {
+        if (items[position].selected) {
+            holder.itemView.setBackgroundColor(context.getColor(android.R.color.holo_green_light))
+
+        } else {
+            holder.itemView.setBackgroundColor(context.getColor(android.R.color.white))
+            //holder.itemView.Description.fontFeatureSettings
+        }
+        holder.bindItems(items[position])
+       // println("${holder.adapterPosition}")
+
+
+
+    }
+
+
+}
